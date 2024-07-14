678c687c82c0e16770626c61fc1d564d6930bff3
raihrmdhn <muhammadraihan3026@gmail.com>
2024-07-14 13:01:21 +0700

    Update FormAnggota.java

--- p040624/FormAnggota.java
+++ p040624/FormAnggota.java
@@ -8,10 +8,7 @@ import javax.swing.JComboBox;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 
-/**
- *
- * @author hafiz
- */
+
 public class FormAnggota extends javax.swing.JFrame {
 
     /**
