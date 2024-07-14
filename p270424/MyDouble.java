97458053135bf78faaf240a0692bea65631890a9
raihrmdhn <muhammadraihan3026@gmail.com>
2024-07-14 14:25:36 +0700

    Update MyDouble.java

--- p270424/MyDouble.java
+++ p270424/MyDouble.java
@@ -4,10 +4,7 @@
  */
 package pbo_2301083018.p270424;
 
-/**
- *
- * @author hafiz
- */
+
 public class MyDouble implements Relation {
     public boolean isGreater(Object a,Object b){
         return (double) a > (double) b;
