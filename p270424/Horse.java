b0b6b178c4cdc9180feb18964fbc2cebab453958
raihrmdhn <muhammadraihan3026@gmail.com>
2024-07-14 14:24:21 +0700

    Update Horse.java

--- p270424/Horse.java
+++ p270424/Horse.java
@@ -4,10 +4,7 @@
  */
 package pbo_2301083018.p270424;
 
-/**
- *
- * @author hafiz
- */
+
 public class Horse extends LivingThing {
     public void walk(){
         System.out.println("Horse walk");
